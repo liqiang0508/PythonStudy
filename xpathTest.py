e9d2846136c02ecdeeba3538f6004433328fee54
liqiang0508 <liqiang497232807@gmail.com>
2019-04-26 17:17:06 +0800

    抓取电影天堂最新电影信息

--- xpathTest.py
+++ xpathTest.py
@@ -3,34 +3,29 @@
 from lxml import etree
 import re
 import os
-html = "<html>\
- <head>\
-  <base href='http://example.com/' />\
-  <title>Example website</title>\
- </head>\
- <body>\
-  <div class='images'>\
-   <a href='image1.html'>Name: My image 1 <br /><img src='image1_thumb.jpg' /></a>\
-   <a href='image2.html'>Name: My image 2 <br /><img src='image2_thumb.jpg' /></a>\
-   <a href='image3.html'>Name: My image 3 <br /><img src='image3_thumb.jpg' /></a>\
-   <a href='image4.html'>Name: My image 4 <br /><img src='image4_thumb.jpg' /></a>\
-   <a href='image5.html'>Name: My image 5 <br /><img src='image5_thumb.jpg' /></a>\
-  </div>\
- </body>\
-</html>"
+import RequestsManager
+URL = "https://www.dytt8.net/"
 
-selector = etree.HTML(html)
+htmldata = RequestsManager.getHtmlData(URL)
+htmldata.encoding = "gb2312"
+selector = etree.HTML(htmldata.text)
 title = selector.xpath("//title/text()")
-print title,title[0]
+print "title-",title[0].encode('gb2312')
 
-image = selector.xpath('//div[@class = "images"]/a')
-print image
-for a in image:
-    print a,a.text,a.get("href"),a.xpath("img")[0].get("src")
 
-p = "56989awadad45454-"
+co_content8 = selector.xpath('//div[@class = "co_content8"]')
 
-pattern = re.compile("(\d+)-")
-result  = pattern.findall(p)
-print result
+tables = co_content8[0].xpath('ul/table/tr')
+print len(tables)
+# print co_content8[0].xpath("string(.)")
+
+if os.path.exists("movie.txt"):
+  os.remove("movie.txt")
+for tr in tables:
+  name = tr.xpath('string(.)').replace("\r\n","")
+  # print name
+  with open("movie.txt","a+") as f:
+    f.write(name.encode('gb2312')+'\n')
+ 
+
 os.system("pause")
