82b7d752ffd6eb2b9be7358b16c144264e70f86a
liqiang0508 <liqiang497232807@gmail.com>
2019-04-29 16:00:46 +0800

    输入地名异常修改

--- awesome-bot/awesome/plugins/weather/data_source.py
+++ awesome-bot/awesome/plugins/weather/data_source.py
@@ -5,4 +5,8 @@ import getCityWeater
 async def get_weather_of_city(city: str) -> str:
     # 这里简单返回一个字符串
     # 实际应用中，这里应该调用返回真实数据的天气 API，并拼接成天气预报内容
-    return f'{city}的天气是:\n'+getCityWeater.GetWeatherByCode(getCityWeater.GetCityCode(str(city)))
+	print("get_weather_of_city----------------",city)
+	if getCityWeater.GetCityCode(city)!=None:
+		return f'{city}的天气是:\n'+getCityWeater.GetWeatherByCode(getCityWeater.GetCityCode(str(city)))
+	else:
+		return "要查询的城市名称不能为空呢，请重新输入"
