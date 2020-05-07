eaf8ea030a81fa0a5b8191ff1d5fbc7686608b4f
Lee0508 <liqiang497232807@gmail.com>
2020-05-07 15:15:47 +0800

    7z 压缩

--- SomeTest/7za.py
+++ SomeTest/7za.py
@@ -5,7 +5,7 @@ def zipFolder(savefile,zipFolder,pwd):
 
 	
 
-	cmd = "7za.exe a -r   "+savefile+" "+zipFolder +"  -mx=9 -mm=LZMA2"
+	cmd = "7za.exe a -r   "+savefile+" "+zipFolder +"  -mx=9 -mm=LZMA"
 	if pwd:
 		cmd = cmd +" -p"+pwd
 	print cmd
@@ -13,6 +13,7 @@ def zipFolder(savefile,zipFolder,pwd):
 
 
 
-# zipFolder("test7.zip","../html/easyui","123")
-# zipFolder("test7.7z","../html/easyui","123")
+zipFolder("test7.zip","../html/easyui","123")
+zipFolder("test7.7z","../html/easyui","123")
+zipFolder("test8.7z","Script_8","123")
 # zipFolder("test7.zip","*.png","123")
