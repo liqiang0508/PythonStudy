706945b9f1984232991278243dfc03a4463fbcdf
Lee0508 <liqiang497232807@gmail.com>
2020-05-27 15:40:03 +0800

    连接池优化

--- SomeTest/MysqlTest.py
+++ SomeTest/MysqlTest.py
@@ -2,40 +2,46 @@
 # -*- coding: UTF-8 -*-
 import pymysql
 import json
-
+from DBUtils.PooledDB import PooledDB
 
 
 # 打开数据库连接
 
-conn=pymysql.connect(
-    host='localhost',
-    port=3306,
-    user='root',
-    password='123456',
-    db='hello',
-    charset='utf8'
-)
+# conn=pymysql.connect(
+#     host='localhost',
+#     port=3306,
+#     user='root',
+#     password='123456',
+#     db='hello',
+#     charset='utf8'
+# )
+pool = PooledDB(pymysql,5,host='localhost',user='root',passwd='123456',db='hello',port=3306, charset="utf8") #5为连接池里的最少连接数
+
 
 
 
 def MySqlSelectAll(sql):
+	conn = pool.connection() #以后每次需要数据库连接就是用connection（）函数获取连接就好了
 	cursor=conn.cursor(cursor=pymysql.cursors.DictCursor)
 	rows=cursor.execute(sql)  # 返回结果是受影响的行数
 	data = cursor.fetchall()
 	# # 关闭游标
 	cursor.close()
+	conn.close()
 	return data
 
 def MySqlSelectOne(sql):
+	conn = pool.connection() #以后每次需要数据库连接就是用connection（）函数获取连接就好了
 	cursor=conn.cursor(cursor=pymysql.cursors.DictCursor)
 	rows=cursor.execute(sql)  # 返回结果是受影响的行数
 	data = cursor.fetchone()
 	# # 关闭游标
 	cursor.close()
+	conn.close()
 	return data
 
 
 
 print MySqlSelectOne("select * from userinfo")
 print MySqlSelectOne("SELECT VERSION()")
-conn.close()
+
