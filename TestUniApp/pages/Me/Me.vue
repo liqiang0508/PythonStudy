76e4e44478b66e43cd9ccaa828effddfbf7cb9b3
Lee0508 <liqiang497232807@gmail.com>
2020-06-02 17:08:21 +0800

    界面

--- TestUniApp/pages/Me/Me.vue
+++ TestUniApp/pages/Me/Me.vue
@@ -3,16 +3,16 @@
 		<view class="myinfo">
 			<view class="myinfo_vertical">
 				<image class="avator" src="../../static/logo.png" mode="scaleToFill"></image>
-				 <view class="myinfo_name">
-						<text>{{userName}}</text>
-						<image class="avator"  style="width: 50px; height: 10px;" src="../../static/logo.png" mode="scaleToFill"></image>
-				 </view>
-				 <view class="addres">
-					  <image style="width: 50rpx; height: 50rpx; margin-left: 15rpx;" src="../../static/logo.png" mode="scaleToFill"></image>
-					  <text style="color: #ffffff; margin-left: 25rpx;">收货地址</text>
-				 </view>
+				<view class="myinfo_name">
+					<text>{{userName}}</text>
+					<image class="avator" style="width: 50px; height: 10px;" src="../../static/logo.png" mode="scaleToFill"></image>
+				</view>
+				<view class="addres">
+					<image style="width: 50rpx; height: 50rpx; margin-left: 15rpx;" src="../../static/logo.png" mode="scaleToFill"></image>
+					<text style="color: #ffffff; margin-left: 25rpx;">收货地址</text>
+				</view>
 			</view>
-			
+
 		</view>
 		<!-- 钱包积分 -->
 		<view class="MJ">
@@ -20,77 +20,100 @@
 				<image style="width: 50rpx; height: 50rpx;" src="../../static/logo.png" mode="scaleToFill"></image>
 				<text>我的钱包</text>
 			</view>
-			
+
 			<view class="money">
 				<text>0</text>
 				<text>积分</text>
 			</view>
 		</view>
-		
-		<view style = "width:100%; height:20rpx; background-color: rgba(0,0,0,0.3);"></view>
-		
-		<view class = "myoder">
+
+		<view style="width:100%; height:20rpx; background-color: rgba(0,0,0,0.3);"></view>
+
+		<view class="myoder">
 			<text style="margin-left:20rpx;">我的订单</text>
 		</view>
-		
-		<hr/>
-		
+
+		<hr />
+
 		<view class="orderinfos">
-				<view class="orderinfo">
-					<image style="width: 50rpx; height: 50rpx;" src="../../static/logo.png" mode="scaleToFill"></image>
-					<text>待付款</text>
-				</view>
-				
-				<view class="orderinfo">
-					<image style="width: 50rpx; height: 50rpx;" src="../../static/logo.png" mode="scaleToFill"></image>
-					<text>待发货</text>
-				</view>
-				
-				<view class="orderinfo">
-					<image style="width: 50rpx; height: 50rpx;" src="../../static/logo.png" mode="scaleToFill"></image>
-					<text>待收货</text>
-				</view>
-				
-				<view class="orderinfo">
-					<image style="width: 50rpx; height: 50rpx;" src="../../static/logo.png" mode="scaleToFill"></image>
-					<text>已完成</text>
-				</view>
-				
-				<view class="orderinfo">
-					<image style="width: 50rpx; height: 50rpx;" src="../../static/logo.png" mode="scaleToFill"></image>
-					<text>售后</text>
-				</view>
+			<view class="orderinfo">
+				<image style="width: 50rpx; height: 50rpx;" src="../../static/logo.png" mode="scaleToFill"></image>
+				<text>待付款</text>
+			</view>
+
+			<view class="orderinfo">
+				<image style="width: 50rpx; height: 50rpx;" src="../../static/logo.png" mode="scaleToFill"></image>
+				<text>待发货</text>
+			</view>
+
+			<view class="orderinfo">
+				<image style="width: 50rpx; height: 50rpx;" src="../../static/logo.png" mode="scaleToFill"></image>
+				<text>待收货</text>
+			</view>
+
+			<view class="orderinfo">
+				<image style="width: 50rpx; height: 50rpx;" src="../../static/logo.png" mode="scaleToFill"></image>
+				<text>已完成</text>
+			</view>
+
+			<view class="orderinfo">
+				<image style="width: 50rpx; height: 50rpx;" src="../../static/logo.png" mode="scaleToFill"></image>
+				<text>售后</text>
+			</view>
 		</view>
-		
-		<view style = "width:100%; height:20rpx; background-color: rgba(0,0,0,0.3);"></view>
+
+		<view style="width:100%; height:20rpx; background-color: rgba(0,0,0,0.3);"></view>
+
+		<uni-list>
+
+			<uni-list-item title="我的拼团" thumb="https://img-cdn-qiniu.dcloud.net.cn/new-page/hx.png"></uni-list-item>
+			<uni-list-item title="我的优惠券" thumb="https://img-cdn-qiniu.dcloud.net.cn/new-page/hx.png"></uni-list-item>
+			<uni-list-item title="领券中心" thumb="https://img-cdn-qiniu.dcloud.net.cn/new-page/hx.png"></uni-list-item>
+			<uni-list-item title="我的收藏" thumb="https://img-cdn-qiniu.dcloud.net.cn/new-page/hx.png"></uni-list-item>
+			<uni-list-item title="在线客服" thumb="https://img-cdn-qiniu.dcloud.net.cn/new-page/hx.png"></uni-list-item>
+			<uni-list-item title="联系我们" thumb="https://img-cdn-qiniu.dcloud.net.cn/new-page/hx.png"></uni-list-item>
+			<uni-list-item title="服务中心" thumb="https://img-cdn-qiniu.dcloud.net.cn/new-page/hx.png"></uni-list-item>
+			<uni-list-item title="秒杀" thumb="https://img-cdn-qiniu.dcloud.net.cn/new-page/hx.png"></uni-list-item>
+			<uni-list-item title="收货地址" thumb="https://img-cdn-qiniu.dcloud.net.cn/new-page/hx.png"></uni-list-item>
+			
+		</uni-list>
+
+		<!-- <view style="height:60px;"></view> -->
 		<!-- <view v-if="listData.length>0">
 			<text>LOl</text>
 		</view> -->
-		
+
 	</view>
 </template>
 
 <script>
+	import uniList from "@/components/uni-list/uni-list.vue"
+	import uniListItem from "@/components/uni-list-item/uni-list-item.vue"
 	export default {
+		components: {
+			uniList,
+			uniListItem
+		},
 		data() {
 			return {
-				userName:"断了的弦的小同学",
-				listData:[]
+				userName: "断了的弦的小同学",
+				listData: []
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
 
 <style>
-	.content{
+	.content {
 		display: flex;
 		flex-direction: column;
 		width: 100%;
 	}
-	.myinfo{
+
+	.myinfo {
 		width: 100%;
 		height: 350rpx;
 		background-color: #ffaa00;
@@ -99,8 +122,8 @@
 		justify-content: center;
 		align-items: center;
 	}
-	
-	.myinfo_vertical{
+
+	.myinfo_vertical {
 		width: 100%;
 		height: 150rpx;
 		/* background-color: #0062CC; */
@@ -109,11 +132,13 @@
 		justify-content: flex-start;
 		align-items: center;
 	}
-	.avator{
+
+	.avator {
 		width: 128rpx;
 		height: 128rpx;
 	}
-	.myinfo_name{
+
+	.myinfo_name {
 		height: 80%;
 		display: flex;
 		flex-direction: column;
@@ -122,30 +147,31 @@
 		align-items: center;
 		margin-left: 20rpx;
 	}
-	
-	.addres{
+
+	.addres {
 		background-color: #ff0000;
 		right: 0rpx;
 		position: absolute;
-		
+
 		width: 210rpx;
 		height: 80rpx;
 		border-radius: 80rpx 0 0 80rpx;
 		display: flex;
 		align-items: center;
 	}
+
 	/* 钱包积分 */
-	.MJ{
+	.MJ {
 		display: flex;
 		flex-direction: row;
 		width: 100%;
 		margin-top: 10rpx;
 		/* background-color: #007AFF; */
 	}
-	
-	.money{
-		width:50%;
-		height:130rpx;
+
+	.money {
+		width: 50%;
+		height: 130rpx;
 		text-align: center;
 		/* background-color: #55aa00; */
 		display: flex;
@@ -153,28 +179,29 @@
 		align-items: center;
 		justify-content: space-around;
 	}
+
 	/* 我的订单 */
-	.myoder{
+	.myoder {
 		width: 100%;
 		height: 90rpx;
 		/* background-color: #0062CC; */
 		display: flex;
 		justify-content: flex-start;
 		align-items: center;
-		color: rgba(0,0,0,1);
-		
+		color: rgba(0, 0, 0, 1);
+
 	}
-	
-	.orderinfos{
+
+	.orderinfos {
 		width: 100%;
 		height: 150rpx;
 		/* background-color: #0062CC; */
 		display: flex;
 		flex-direction: row;
-		
+
 	}
-	
-	.orderinfo{
+
+	.orderinfo {
 		width: 20%;
 		height: 100%;
 		/* background-color: #3F536E; */
