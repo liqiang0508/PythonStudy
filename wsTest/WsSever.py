8f062575179f7c14a88e2816d92b00f5f21ce73d
Lee0508 <liqiang497232807@gmail.com>
2020-06-15 17:53:09 +0800

    no message

--- wsTest/WsSever.py
+++ wsTest/WsSever.py
@@ -12,7 +12,7 @@ import messagedispatch
 def send_to(server,client,message):
 
    server.send_message_to_all(client,message)
-   
+
 #广播给所有人
 def send_to_all(server,message):
 
@@ -24,28 +24,29 @@ def new_client(client, server):
    # print("New client connected and was given id %d" % client['id'])
    server.send_message_to_all("Hey all, a new client has joined us")
    SetPlayerUid(724001)
+   logger.info("Client(%d) connected" % (client['uid']))
 
 #连接后客户端请求下设置uid
 def SetPlayerUid(id):
 
-   print "SetPlayerUid----------",id
+   # print "SetPlayerUid----------",id
    server.clients[len(server.clients)-1]["uid"] = id
-   print server.clients
+   # print server.clients
    
 # Called for every client disconnecting
 def client_left(client, server):
 
-   print("Client(%d) disconnected" % client['id'])
-   logger.info("Client(%d) leave" % (client['id']))
+   # print("Client(%d) disconnected" % client['uid'])
+   logger.info("Client(%d) disconnected" % (client['uid']))
 
   
 
 # Called when a client sends a message
 def message_received(client, server, message):
 
-   print("Client(%d) said: %s" % (client['id'], message))
-   logger.info("Client(%d) said: %s" % (client['id'], message))
-   message = "Client(%d) said: %s" % (client['id'], message)
+   # print("Client(%d) said: %s" % (client['uid'], message))
+   logger.info("message_received(%d) said: %s" % (client['uid'], message))
+   message = "Client(%d) said: %s" % (client['uid'], message)
    messagedispatch.message_received(client, server, message)
 
   
@@ -58,12 +59,12 @@ if __name__ == "__main__":
    logger = logging.getLogger(__name__)
    logger.setLevel(level = logging.DEBUG)
    handler = logging.FileHandler("{}-{}.txt".format(day,port))
-   handler.setLevel(logging.INFO)
+   handler.setLevel(logging.DEBUG)
    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
    handler.setFormatter(formatter)
    logger.addHandler(handler)
 
-   server = WebsocketServer(host = "0.0.0.0",port = port)
+   server = WebsocketServer(host = "0.0.0.0",port = port,loglevel=logging.INFO)
    server.set_fn_new_client(new_client)
    server.set_fn_client_left(client_left)
    server.set_fn_message_received(message_received)
