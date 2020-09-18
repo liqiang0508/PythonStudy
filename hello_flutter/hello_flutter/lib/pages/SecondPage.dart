a3b96d1557338dd1b25211db22b82afbe7671c7c
Lee0508 <liqiang497232807@gmail.com>
2020-09-18 17:22:13 +0800

    test

--- hello_flutter/hello_flutter/lib/pages/SecondPage.dart
+++ hello_flutter/hello_flutter/lib/pages/SecondPage.dart
@@ -3,9 +3,42 @@
  * @version: 
  * @Author: Lee
  * @Date: 2020-09-17 15:09:18
- * @LastEditTime: 2020-09-18 16:50:01
+ * @LastEditTime: 2020-09-18 17:20:56
  */
 import 'package:flutter/material.dart';
+
+final listData = [
+  {
+    "name": "Lee",
+    "imgurl":
+        "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1373560079,871367259&fm=26&gp=0.jpg"
+  },
+  {
+    "name": "Lee",
+    "imgurl":
+        "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1373560079,871367259&fm=26&gp=0.jpg"
+  },
+  {
+    "name": "Lee",
+    "imgurl":
+        "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1373560079,871367259&fm=26&gp=0.jpg"
+  },
+  {
+    "name": "Lee",
+    "imgurl":
+        "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1373560079,871367259&fm=26&gp=0.jpg"
+  },
+  {
+    "name": "Lee",
+    "imgurl":
+        "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1373560079,871367259&fm=26&gp=0.jpg"
+  },
+  {
+    "name": "Lee",
+    "imgurl":
+        "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1373560079,871367259&fm=26&gp=0.jpg"
+  }
+];
 
 class SecondPage extends StatelessWidget {
   @override
@@ -15,17 +48,23 @@ class SecondPage extends StatelessWidget {
         title: new Text('SecondPage'),
         centerTitle: true,
       ),
-      body: ListView(children: <Widget>[]),
-      // body: Center(
-      //     child: RaisedButton(
-      //         child: Text("go MainPage"),
-      //         onPressed: () {
-      //           Navigator.push(context, MaterialPageRoute(builder: (_) {
-      //             return new Home();
-      //           }));
-      //         })),
+      body: ListView(
+          children: listData
+              .map((e) => Column(
+                    children: <Widget>[
+                      ListTile(
+                          title: Text(e["name"]),
+                          leading: Image.network(e["imgurl"])),
+                      Divider(
+                        thickness: 1,
+                      )
+                    ],
+                  ))
+              .toList()),
     );
   }
 
   // _onclick() {}
 }
+// ListTile(
+//                   title: Text(e["name"]), leading: Image.network(e["imgurl"])))
