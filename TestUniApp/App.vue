902e3920fb1a52c807b2f761cae53c286c7c635e
Lee0508 <liqiang497232807@gmail.com>
2020-10-21 11:03:18 +0800

    ..

--- TestUniApp/App.vue
+++ TestUniApp/App.vue
@@ -1,16 +1,23 @@
 <script>
 	import HttpHelper from "common/HttpHelper.js"
 	import VersionManager from "common/VersionManager.js"
+	
 	export default {
 		onLaunch: function() {
 			console.log('App Launch')
 			var url = "http://192.168.65.172/hotupversion/uniconfigrelease"
 			//#ifdef APP-PLUS 
-				VersionManager.checkUpdate(url)
+				VersionManager.checkUpdate(url,(res)=>{
+					var percent = res.progress//进度
+					console.log("下载进度=",percent)
+				},(code)=>{
+					console.log("更新结束返回code ===",code)
+				})
 			//#endif
 		},
 		onShow: function() {
 			console.log('App Show')
+		
 			
 		},
 		onHide: function() {
