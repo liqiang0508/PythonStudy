52f521f47cb2217079ad621d863ffb43ebbfdc6e
Lee0508 <liqiang497232807@gmail.com>
2020-10-26 15:06:26 +0800

    复制资源到AS

---
+++ TestUniApp/copyAssetToAs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+import shutil
+print(__file__)
+print("copyAssetsToAs")
+
+
+#复制src目录下面所有的文件到 dst目录下面 
+def copyFileTree(src,dst ): 
+	if os.path.exists(dst):
+		print dst+"  delete**** "
+		shutil.rmtree(dst)
+	else:
+		pass
+			
+
+	shutil.copytree(src, dst)
+	print "copy tree %s -> %s"%( src,dst)
+	# return
+
+
+sourcefolder = "unpackage/resources/__UNI__37FA08B/www"
+targetfolder = "AndroidProject/app/src/main/assets/apps/__UNI__37FA08B/www"
+copyFileTree(sourcefolder,targetfolder)
+
+print("copyAssetsToAs--------------end")
+
+os.system("pause")
