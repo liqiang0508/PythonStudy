8cf86cde1f80a01ddb5c5ecdc045f27e1ea8e4e8
Lee0508 <liqiang497232807@gmail.com>
2021-01-18 15:15:48 +0800

    fix

--- TornadoTest/tornadotest.py
+++ TornadoTest/tornadotest.py
@@ -3,6 +3,8 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 import os
+import datetime
+
 
 UPLOADPATH = "static/uploadfile"#上传文件夹名称
 PORT = 8888 #端口
@@ -57,6 +59,8 @@ class UpLoadFileSuccess(tornado.web.RequestHandler):
 
 # #websocket
 class WebScocketHandler(tornado.websocket.WebSocketHandler) :
+
+    users = set()  # 用来存放在线用户的容器
     def check_origin(self, origin) :
         '''重写同源检查 解决跨域问题'''
         return True
@@ -64,16 +68,21 @@ class WebScocketHandler(tornado.websocket.WebSocketHandler) :
     def open(self) :
         '''新的websocket连接后被调动'''
         print("on_open")
-        self.write_message('Welcome')
+        self.users.add(self)  # 建立连接后添加用户到容器中
+        for user in self.users:  # 向已在线用户发送消息
+            user.write_message(u"[%s]-[%s]-进入聊天室" % (self.request.remote_ip, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
 
     def on_close(self) :
         print("on_close")
-        '''websocket连接关闭后被调用'''
+        self.users.remove(self) # 用户关闭连接后从容器中移除用户
+        for user in self.users:
+            user.write_message(u"[%s]-[%s]-离开聊天室" % (self.request.remote_ip, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
 
     def on_message(self, message) :
         '''接收到客户端消息时被调用'''
         print("on_message==",message)
-        self.write_message('new message :' + message)  # 向客服端发送
+        for user in self.users:  # 向在线用户广播消息
+            user.write_message(u"[%s]-[%s]-说：%s" % (self.request.remote_ip, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), message))
 
 def omMessageCall(msg):
     print(msg)
