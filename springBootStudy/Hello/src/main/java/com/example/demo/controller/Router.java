5721d2f93453d5076fc9e51078b0ca50dc43794d
Lee <497232807@qq.com>
2021-06-07 18:10:31 +0800

    运行时修改文件 自动更新

--- springBootStudy/Hello/src/main/java/com/example/demo/controller/Router.java
+++ springBootStudy/Hello/src/main/java/com/example/demo/controller/Router.java
@@ -4,13 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class Index {
+public class Router {
 
     @RequestMapping("/index")
-    public String sayHello() {
+    public String Index() {
         return "Index"; //
 //        http://localhost:8888/Hello.html 返回static下面的静态html
 //        http://localhost:8888/index      返回templates下面的动态html
     }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "Login";
+    }
+
 
 }
