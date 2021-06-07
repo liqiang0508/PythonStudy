c75f513209e4e42907e5d42fe7ed51a4683c5d1c
Lee <497232807@qq.com>
2021-06-07 15:11:30 +0800

    spring boot  layui

--- springBootStudy/Hello/src/main/java/com/example/demo/DemoApplication.java
+++ springBootStudy/Hello/src/main/java/com/example/demo/DemoApplication.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
@@ -27,6 +29,11 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String index () {
+		return "Hello";
+	}
+
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
@@ -36,5 +43,12 @@ public class DemoApplication {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+	@GetMapping("getTime")
+	public long getTime(){
+		long time = new Date().getTime();
+
+		return time;
+	}
+
 }
             
--- springBootStudy/Hello/src/main/java/com/example/demo/controller/Index.java
+++ springBootStudy/Hello/src/main/java/com/example/demo/controller/Index.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class Home {
+public class Index {
 
     @RequestMapping("/index")
     public String sayHello() {
-        return "Hello"; //
+        return "Index"; //
 //        http://localhost:8888/Hello.html 返回static下面的静态html
 //        http://localhost:8888/index      返回templates下面的动态html
     }
