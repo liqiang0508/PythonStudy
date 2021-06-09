8f82ab16cf8906da46f882f1c01ed7a3479ab928
Lee0508 <liqiang497232807@gmail.com>
2021-06-09 21:12:59 +0800

    类来接收 参数

--- springBootStudy/Hello/src/main/java/com/example/demo/controller/HelloController.java
+++ springBootStudy/Hello/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,18 +1,17 @@
 package com.example.demo.controller;
 
 import com.example.common.Greeting;
+import com.example.common.Person;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicLong;
 
-
+@Slf4j
 @RestController
 public class HelloController {
 
@@ -24,21 +23,27 @@ public class HelloController {
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
-
+    //用类来接收参数
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public Greeting greeting(Person person) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, person.toString()));
     }
 
     @GetMapping("getTime")
     public long getTime() {
         long time = new Date().getTime();
-
         return time;
     }
 
     @PostMapping("/login")
     public String login(@RequestParam("title") String title, @RequestParam("password") String Pwd) {
-        return title + ":" + Pwd;
+        log.info("login==="+title+":"+Pwd);
+        if(Pwd=="123456")
+        {
+            return title + ":" + Pwd;
+        }
+
+        return "err login";
+
     }
 }
