b19d5a7758bff5f43d6f622ace495f1290f4b6af
Lee <497232807@qq.com>
2021-07-20 17:22:52 +0800

    配置文件配置mongo 地址

--- springBootStudy/Hello/src/main/java/com/example/demo/config/AppConfig.java
+++ springBootStudy/Hello/src/main/java/com/example/demo/config/AppConfig.java
@@ -1,22 +1,22 @@
-package com.example.demo.config;
-
-
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
-
-@Configuration
-public class AppConfig {
-
-    public @Bean
-    MongoClient mongoClient() {
-        return MongoClients.create("mongodb://localhost:27017");
-    }
-
-    public @Bean
-    MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoClient(), "hello");
-    }
-}
+//package com.example.demo.config;
+//
+//
+//import com.mongodb.client.MongoClient;
+//import com.mongodb.client.MongoClients;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.mongodb.core.MongoTemplate;
+//
+//@Configuration
+//public class AppConfig {
+//
+//    public @Bean
+//    MongoClient mongoClient() {
+//        return MongoClients.create("mongodb://Lee:201162@localhost:27017/hello");
+//    }
+//
+//    public @Bean
+//    MongoTemplate mongoTemplate() {
+//        return new MongoTemplate(mongoClient(), "hello");
+//    }
+//}
