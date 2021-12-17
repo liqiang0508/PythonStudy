36b479764eb340a19d6443bb6122fde5233811c1
Lee <497232807@qq.com>
2021-12-17 09:30:23 +0800

    requirement.txt

--- salaryEmail/sendEamil.bat
+++ salaryEmail/sendEamil.bat
@@ -4,7 +4,7 @@ if defined fileName (
     echo export file %fileName% 
     @REM 参数isTest( 是否是测试模式) 0 就是正式发送邮件，1 就是只打印邮件内容
     call sendSalaryEmail.py %fileName%  0
-) else ( echo "Usage: run.bat <fileName>" )
+) else ( echo "Usage: run.bat <fileName> isTest" )
 
 
-Pause
+pause
