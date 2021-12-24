5478d4670f6bf39a43ec8f9ff424c054c77d3138
Lee <497232807@qq.com>
2021-12-24 15:01:17 +0800

    build cmd ts

---
+++ protoTool/buildCmdTs.py
@@ -0,0 +1,59 @@
+'''
+Descripttion: 
+version: 
+Author: liqiang
+email: 497232807@qq.com
+Date: 2021-12-22 19:48:46
+LastEditTime: 2021-12-24 14:59:57
+'''
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+import shutil
+
+cmdFile = "cmd.cfg"
+writeStr = "//@ts-nocheck\n"
+writeStr = writeStr + "var CMD={};\n"
+writeStr = writeStr + "var CMD2PB={};\n"
+
+# 移动文件
+def moveFile(srcfile, dstfile):
+    if not os.path.isfile(srcfile):
+        print ("%s not exist!" % (srcfile))
+    else:
+        fpath, fname = os.path.split(dstfile)  # 分离文件名和路径
+        if not os.path.exists(fpath):
+            os.makedirs(fpath)  # 创建路径
+        shutil.move(srcfile, dstfile)
+        
+with open(cmdFile,"r+") as f:
+    while True:
+        text_line = f.readline()
+        if text_line:
+            if "#" not in text_line and len(text_line)>1:
+                text_line = text_line.replace("\t","")
+                text_line = text_line.replace("\n","")
+                text_line = text_line.split(",")
+                name = text_line[0]
+                number = text_line[1]
+                fileName = text_line[2]
+                pak = text_line[3]
+                writeStr = writeStr + "CMD.{} = {}\n".format(name,number)
+                writeStr = writeStr + "CMD2PB[CMD.{}]={{ name:\"{}\", pak:\"{}\", file:\"{}\" }}\n".format(name,name,pak,fileName)
+                # print(text_line)
+        else:
+            break 
+
+writeStr = writeStr + "\nglobalThis.CMD = CMD\n"
+writeStr = writeStr + "globalThis.CMD2PB = CMD2PB\n"
+with open("cmd.ts","w") as f:
+    f.write(writeStr)
+    f.close()
+
+srcFile = "cmd.lua"
+dstFile = "lua/cmd.lua"
+# moveFile(srcFile, dstFile)
+
+print("build cmd ts success================")
+os.system("pause")
--- protoTool/lua/cmd.lua
+++ protoTool/lua/cmd.lua
@@ -3,8 +3,8 @@ CMD = CMD or{}
 cc.exports.CMD2PB = CMD2PB or {}
 CMD2PB = CMD2PB or {}
 CMD.Login = 100
-CMD2PB[CMD.Login] ={ name= "Login", pak = "com.test.message", file = "a.pb" }
-CMD.Login1 = 200
-CMD2PB[CMD.Login1] ={ name= "Login1", pak = "com.test.message", file = "a.pb" }
-CMD.Login2 = 300
-CMD2PB[CMD.Login2] ={ name= "Login2", pak = "com.test.message", file = "a.pb" }
+CMD2PB[CMD.Login] ={ name= "Login", pak = "tutorial.Person", file = "addressbook.pb" }
+CMD.Login1 = 101
+CMD2PB[CMD.Login1] ={ name= "Login1", pak = "tutorial.Person", file = "addressbook.pb" }
+CMD.Login2 = 102
+CMD2PB[CMD.Login2] ={ name= "Login2", pak = "tutorial.Person", file = "addressbook.pb" }
