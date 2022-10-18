dc6ecc523b4c3ca6a7b408217778d4c9fbece46f
Lee <497232807@qq.com>
2022-10-18 15:52:27 +0800

    del 导表文件夹

--- buildExcle2Lua/sheets_Ts/ConstDefineConfig.ts
+++
@@ -1,4 +0,0 @@
-export default {
-["name"]:{ID: "name", Value: "666.0"},
-["age"]:{ID: "age", Value: "888.0"},
-}
--- buildExcle2Lua/sheets_Ts/PersonConfig.ts
+++
@@ -1,3 +0,0 @@
-export default {
-[1001]:{ID: 1001, Name: "Lee", Hobby: ["movie","music"], Salary: 32.1, Pets: {PetName: "cat", PetAge: 22}},
-}
