32a1c6f2d0b19c85c32ec2dad1db01653f162185
lee <497232807@qq.com>
2025-06-17 14:47:15 +0800

    pandas study

--- pandasStudy/Test.py
+++ pandasStudy/Test.py
@@ -21,6 +21,34 @@ print ("********************************************")
 #找打age大于12的行 并把age+2
 df.loc[df["Age"] > 12, "Age"] = df["Age"] + 2
 print(df)
+df.to_excel("data.xlsx")  # 保存到Excel文件
+print ("********************************************")
 
-df.to_excel("data.xlsx")
+# # 示例数据
+# left = pd.DataFrame({'ID': [1, 2, 3], 'Name': ['Alice', 'Bob', 'Charlie']})
+# right = pd.DataFrame({'ID': [1, 2, 4], 'Age': [24, 27, 22]})
+# # 使用 merge 进行内连接
+# result = pd.merge(left, right, on='ID', how='inner')
+# print(result)
 
+
+
+# 示例数据
+# df1 = pd.DataFrame({'A': [1, 2, 3]})
+# df2 = pd.DataFrame({'A': [4, 5, 6]})
+# # 行合并
+# # objs	需要合并的 DataFrame 列表
+# # axis	合并的轴，0 表示按行合并，1 表示按列合并
+# # ignore_index	是否忽略索引，重新生成索引（默认为 False）
+# # keys	为合并的对象提供层次化索引
+# result = pd.concat([df1, df2], axis=0, ignore_index=True)
+# print(result)
+
+
+# 示例数据
+# left = pd.DataFrame({'A': [1, 2, 3]}, index=[1, 2, 3])
+# right = pd.DataFrame({'B': [4, 5, 6]}, index=[1, 2, 4])
+
+# # 使用 join 进行连接
+# result = left.join(right, how='outer')
+# print(result)
